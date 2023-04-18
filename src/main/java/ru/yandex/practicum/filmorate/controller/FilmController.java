6ff0fbc2b3db57a37a2b242e505fb8d6de41b49e
Pisarev Alexander <Wr1terWr1ter@yandex.ru>
2023-04-18 22:54:35 +0700

    Sprint #9

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequestMapping("/films")
+public class FilmController {
+
+    private int id = 1;
+    private Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+       if (validator(film) && !films.containsKey(film.getId())) {
+           films.put(film.getId(), film);
+           log.info("Добавлен новый фильм: {}", film);
+       }
+       return films.get(film.getId());
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film film) {
+        if (validator(film) && films.containsKey(film.getId())) {
+            films.put(film.getId(), film);
+            log.info("Данные фильма обновлены: {}", film);
+        } else {
+            log.warn("Ошибка обновления: id {} не найден", film.getId());
+            throw new ValidationException("id " + film.getId() + "не найден");
+        }
+        return films.get(film.getId());
+    }
+
+    public boolean validator(Film film) {
+        if (film.getName().isBlank()) {
+            log.warn("Ошибка валидации: name не может быть пустым");
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (film.getDescription().length() > 200) {
+            log.warn("Ошибка валидации: длина description {} знаков", film.getDescription().length());
+            throw new ValidationException("Описание не должно быть длиннее 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Ошибка валидации: releaseDate {} что раньше 1895-12-28", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не может быть раньше 1895-12-28");
+        }
+        if (film.getDuration() <= 0) {
+            log.warn("Ошибка валидации: duration {} меньше 0", film.getDuration());
+            throw new ValidationException("Продолжительность фильма должна быть больше 0");
+        }
+        if (film.getId() == 0) {
+            film.setId(id);
+            id++;
+        } else if (id == film.getId()) {
+            id++;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+
+    private int id = 1;
+    private Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        if (validator(user) && !users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            log.info("Добавлен новый пользователь: {}", user);
+        }
+        return users.get(user.getId());
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        if (validator(user) && users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            log.info("Данные пользователя обновлены: {}", user);
+        } else {
+            log.warn("Ошибка обновления: id {} не найден", user.getId());
+            throw new ValidationException("id " + user.getId() + "не найден");
+        }
+        return users.get(user.getId());
+    }
+
+    public boolean validator(User user) {
+        if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Ошибка валидации: email {} не корректный", user.getEmail());
+            throw new ValidationException("Не корректный email");
+        }
+        if (user.getLogin().isBlank()) {
+            log.warn("Ошибка валидации: login {} не корректный", user.getLogin());
+            throw new ValidationException("Не корректный логин");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Ошибка валидации: birthday {} не корректный", user.getBirthday());
+            throw new ValidationException("Не корректная дата рождения");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getId() == 0) {
+            user.setId(id);
+            id++;
+        } else if (id == user.getId()) {
+            id++;
+        }
+        return true;
+    }
+}
